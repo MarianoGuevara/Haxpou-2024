20e1327f28fbe41440de780495b97b74a4182c46
MarianoGuevara <marianoguevara2005@gmail.com>
2024-10-28 16:27:57 -0300

    login diseño completo

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonButton, IonInput, IonLabel, IonItem, IonCard, IonContent  } from '@ionic/angular/standalone';
+import { IonButton, IonInput, IonLabel, IonItem, IonCard, IonContent, IonInputPasswordToggle  } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, IonButton, IonInput, IonLabel, IonItem, IonCard, IonContent ]
+  imports: [CommonModule, FormsModule, IonButton, IonInput, IonLabel, IonItem, IonCard, IonContent, IonInputPasswordToggle ]
 })
 export class LoginPage implements OnInit {
 
