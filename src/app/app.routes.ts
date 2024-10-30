b61d387d859c8184a262f5ea264819aed00c73eb
Valentin Gradia <valentingonzalezgradia@gmail.com>
2024-10-30 16:59:19 -0300

    Diseño de altas hecho (faltaria darle interactividad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: 'home',
-        loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
-    },
-    {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full',
-    },
+  {
+      path: 'home',
+      loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
+  },
+  {
+      path: '',
+      redirectTo: 'alta-ds', //modificar esto
+      pathMatch: 'full',
+  },
+  {
+    path: 'alta-ds',
+    loadComponent: () => import('./pages/alta-ds/alta-ds.page').then( m => m.AltaDSPage)
+  },
+
 ];
