6b9fd718a11a9047a4aa88baa6fe387b8f01c18d
Franco Jimenez Copati <fjcopati@gmail.com>
2024-11-20 16:34:39 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'AIzaSyB6z7efyK10PQSIUQXV-RHpmE-JJN23vQ8',
-        authDomain: 'haxpou-2024.firebaseapp.com',
-        projectId: 'haxpou-2024',
-        storageBucket: 'haxpou-2024.appspot.com',
-        messagingSenderId: '419100857783',
-        appId: '1:419100857783:web:467907d7abc76c9eba8432',
+        hided
     },
 };
 
